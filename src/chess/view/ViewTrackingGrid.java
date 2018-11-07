92fb5458058b71dfb4de5264e3804560d7064990
Brendan Roberts <kroberts5@stu.jsu.edu>
2018-11-07 14:07:22 -0600

    Added the resources package and some of the images for the pieces

--- src/chess/view/ViewTrackingGrid.java
+++ src/chess/view/ViewTrackingGrid.java
@@ -23,6 +23,18 @@ public class ViewTrackingGrid extends JPanel implements AbstractView {
     private final DefaultController controller;
     
     private static final String ICON_ROOT = "/resources/images/";
+    private static final String WHITE_PAWN = "white_pawn";
+    private static final String WHITE_ROOK = "white_rook";
+    private static final String WHITE_BISHOP = "white_bishop";
+    private static final String WHITE_KNIGHT = "white_knight";
+    private static final String WHITE_QUEEN = "white_queen";
+    private static final String WHITE_KING = "white_king";
+    private static final String BLACK_PAWN = "black_pawn";
+    private static final String BLACK_ROOK = "black_rook";
+    private static final String BLACK_BISHOP = "black_bishop";
+    private static final String BLACK_KNIGHT = "black_knight";
+    private static final String BLACK_QUEEN = "black_queen";
+    private static final String BLACK_KING = "black_king";
     
     private Color beige;
     private Color brown;
@@ -74,6 +86,23 @@ public class ViewTrackingGrid extends JPanel implements AbstractView {
     }
     private void initImages(){
         images = new HashMap();
+        
+        whitePawn = new ImageIcon(getClass().getResource(ICON_ROOT + WHITE_PAWN));
+        whiteRook = new ImageIcon(getClass().getResource(ICON_ROOT + WHITE_ROOK));
+        whiteBishop = new ImageIcon(getClass().getResource(ICON_ROOT + WHITE_BISHOP));
+        
+        images.put(WHITE_PAWN, whitePawn);
+        images.put(WHITE_ROOK, whiteRook);
+        images.put(WHITE_BISHOP, whiteBishop);
+        
+        blackPawn = new ImageIcon(getClass().getResource(ICON_ROOT + BLACK_PAWN));
+        blackRook = new ImageIcon(getClass().getResource(ICON_ROOT + BLACK_ROOK));
+        blackBishop = new ImageIcon(getClass().getResource(ICON_ROOT + BLACK_BISHOP));
+        
+        images.put(BLACK_PAWN, blackPawn);
+        images.put(BLACK_ROOK, blackRook);
+        images.put(BLACK_BISHOP, blackBishop);
+        
     }
     
     private void spaceClicked(){
@@ -87,7 +116,7 @@ public class ViewTrackingGrid extends JPanel implements AbstractView {
             String pieceType = ((BoardSpace)e.getNewValue()).getPiece().getType().toUpperCase();
             String color = ((BoardSpace)e.getNewValue()).getPiece().getColor().toUpperCase();
             
-            String imageType = color + "_" + pieceType;
+            String imageType = color.toLowerCase() + "_" + pieceType.toLowerCase();
             ImageIcon image = images.get(imageType);
             grid[x][y].setIcon(image);
         }
